f93affdc07711e76e3b35d6007833fed669db644
Keerthana <153195226+pk9491@users.noreply.github.com>
2024-04-10 05:00:16 -0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic=[
+    {
+        name:"My Universe",
+        artist:"BTS and Coldplay",
+        img:"music-1",
+        src:"music-1"
+    },
+    {
+        name:"Who Says",
+        artist:"Selena Gomez",
+        img:"music-2",
+        src:"music-2"
+    },
+    {
+        name:"Night Changes",
+        artist:"One Direction",
+        img:"music-3",
+        src:"music-3"
+    },
+    {
+        name:"Love Story",
+        artist:"Taylor Swift",
+        img:"music-4",
+        src:"music-4"
+    },
+    {
+        name:"Cheri Cheri Lady",
+        artist:"Modern Talking",
+        img:"music-5",
+        src:"music-5"
+    },
+    {
+        name:"OK Not to Be OK",
+        artist:"Demi Lovato and Marshmello",
+        img:"music-6",
+        src:"music-6"
+    }
+]
